84e6841069c606fa99837de698f30925886051f1
wooyeonwon <dndus567@naver.com>
2022-08-23 18:40:56 +0900

    환경세팅 완료

---
+++ src/main/java/com/web/testboards/TestBoardsApplication.java
@@ -0,0 +1,13 @@
+package com.web.testboards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBoardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestBoardsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/web/testboards/TestBoardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.testboards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBoardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
